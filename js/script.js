4028812db84f6a86ef23775befae654ad359784b
Milos Mijajlovic <milos92mijajlovic@gmail.com>
2022-08-10 18:56:41 +0200

    script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+//getting all required elements
+const buttonIntr = document.querySelector(".introductionButton");
+
+//If start Button Clicked
+buttonIntr.onclick = () => {
+  //because in question.js we have only one questions object, we must put zero(0) in showQuestions function
+  showQuestions(0);
+};
 //getting questions and options from array
 
 let que_count = 0;
@@ -7,9 +15,17 @@ function showQuestions(index) {
   const option_list = document.querySelector(".option_list");
   let que_tag = "<span>" + questions[index].question + "</span>";
   let option_tag =
-    '<div class="option"></span></div>' +
-    '<div class="option"></span></div>' +
-    '<div class="option"></span></div>' +
-    '<div class="option"></span></div>';
+    //
+    '<div class="option">' +
+    questions[index].options[0] +
+    "<span></span></div>";
+  //
+  '<div class="option">' + questions[index].options[1] + "<span></span></div>";
+  //
+  '<div class="option">' + questions[index].options[2] + "<span></span></div>";
+  //
+  '<div class="option">' + questions[index].options[3] + "<span></span></div>";
+
   que_text.innerHTML = que_tag;
+  option_list.innerHTML = option_tag;
 }
