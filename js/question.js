eea678214c919d417c25ed4d4f1504d13af3e2c8
Milos Mijajlovic <milos92mijajlovic@gmail.com>
2022-08-12 18:38:06 +0200

    next button added

--- js/question.js
+++ js/question.js
@@ -10,7 +10,19 @@ let questions = [
         "Hyper Text Multiple Language.",
         "Hyper Tool Multi Language.",
       ],
-    },
+  },
+  
+  {
+    numb: 2,
+    question: "What does Css stand for?",
+    answer: "Hyper Text Markup Language.",
+    options: [
+      "Cascading style sheet.",
+      "Hyper Text Markup Language.",
+      "Hyper Text Multiple Language.",
+      "Hyper Tool Multi Language.",
+    ],
+  },
 
     //we can put many objects like this
   ];
--- js/script.js
+++ js/script.js
@@ -14,16 +14,40 @@ window.onload = function () {
 //getting questions and options from array
 
 let que_count = 0;
+let que_numb = 1;
+//getting the button next
+const nextButton = document.querySelector(".nextButton");
+//if nextButton clicked
+nextButton.onclick = () => {
+  if (que_count < questions.length - 1) {
+    que_count++;
+    que_numb++;
+    showQuestions(que_count);
+    queCounter(que_numb);
+  } else {
+    console.log("Question completed");
+  }
+};
 
+//function for showing questions
 function showQuestions(index) {
+  //question getting query
   const que_text = document.querySelector(".que_text");
+  //list of answer getting query
   const option_list = document.querySelector(".option_list");
-  let que_tag = "<p>" + `${questions[index].question}` + "</p>";
+  let que_tag =
+    "<p>" +
+    `${questions[index].numb}` +
+    "." +
+    `${questions[index].question}` +
+    "</p>";
   let option_tag = `
     
-    <div class="option">${questions[index].options[0]} <span></span></div>
+    <div class="option">${questions[index].options[0]} <span></span> </div>
+   
   
   <div class="option"> ${questions[index].options[1]}<span></span></div>
+  
   
   <div class="option">${questions[index].options[2]}<span></span></div>
   
@@ -31,4 +55,30 @@ function showQuestions(index) {
 
   que_text.innerHTML = que_tag;
   option_list.innerHTML = option_tag;
+  //option list loop array
+  const option = option_list.querySelectorAll(".option");
+  for (let i = 0; i < option.length; i++) {
+    //we grab all option and put them onclick event
+    option[i].setAttribute("onclick", "optionSelected(this)");
+  }
+}
+
+//qounter question
+
+function queCounter(index) {
+  const queCounter = document.querySelector(".queCounter");
+  let totalQuesCountTag =
+    "<span><h2>" + index + "/" + questions.length + "</h2></span>";
+
+  queCounter.innerHTML = totalQuesCountTag;
+}
+
+//function option selected answer
+
+function optionSelected(answer) {
+  let userAns = answer.textContent;
+  let correctAns = questions[que_count].answer;
+  if (userAns == correctAns) {
+    console.log("Answer is Correct");
+  } else "Answer is Wrong";
 }
