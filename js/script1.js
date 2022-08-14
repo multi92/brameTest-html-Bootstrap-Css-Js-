d049b8735bdcff40ba3b9796b898b074cb86cc4b
Milos Mijajlovic <milos92mijajlovic@gmail.com>
2022-08-14 19:46:47 +0200

    script1.js added

--- js/script1.js
+++ js/script1.js
@@ -1,100 +1,100 @@
-var startButton = document.getElementById('start-btn')
-const nextButton = document.getElementById('next-btn')
-const questionContainerElement = document.getElementById('question-container');
-const questionElement = document.getElementById('question')
-const answerButtonsElement = document.getElementById('answer-buttons')
-const queCounter = document.getElementById('queCounter')
+var startButton = document.getElementById("start-btn");
+const nextButton = document.getElementById("next-btn");
+const questionContainerElement = document.getElementById("question-container");
+const questionElement = document.getElementById("question");
+const answerButtonsElement = document.getElementById("answer-buttons");
+const queCounter = document.getElementById("queCounter");
 
-var shuffledQuestions, currentQuestionIndex
+//start with undefined
+var shuffledQuestions, currentQuestionIndex;
 
 var clicks = 1;
-startButton.addEventListener('click', ()=>{
-  
-  startButton.classList.add('hide')
-  shuffledQuestions = questions.sort(() => Math.random() - .5)
+startButton.addEventListener("click", () => {
+  //when button is clicked hide-button
+  startButton.classList.add("hide");
+  //random questions if we have more questions (shuffle with math random funcitons)
+  shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+  //first question
   currentQuestionIndex = 0;
-  questionContainerElement.classList.remove('hide')
-  setNextQuestion()
+  //remowe hide
+  questionContainerElement.classList.remove("hide");
+  setNextQuestion();
 });
 
-nextButton.addEventListener('click', () => {
-  currentQuestionIndex++
-  setNextQuestion()
+nextButton.addEventListener("click", () => {
+  currentQuestionIndex++;
+  setNextQuestion();
 });
-
-
-
 
 document.body.onload = function () {
   currentQuestionIndex = 0;
-  currentQuestionIndex++
+  currentQuestionIndex++;
 
   queCounter.innerHTML = `
     Question ${clicks} / ${questions.length}
   `;
-}
+};
 
 function setNextQuestion() {
-  resetState()
-  showQuestion(shuffledQuestions[currentQuestionIndex])
+  resetState();
+  showQuestion(shuffledQuestions[currentQuestionIndex]);
 }
 
 function showQuestion(question) {
-  questionElement.innerText = question.question
-  question.answers.forEach(answer => {
-    const button = document.createElement('button')
-    button.innerText = answer.text
-    button.classList.add('btn')
+  questionElement.innerText = question.question;
+  question.answers.forEach((answer) => {
+    const button = document.createElement("button");
+    button.innerText = answer.text;
+    button.classList.add("btn");
     if (answer.correct) {
-      button.dataset.correct = answer.correct
+      button.dataset.correct = answer.correct;
     }
-    button.addEventListener('click', selectAnswer)
-    answerButtonsElement.appendChild(button)
-  })
+    button.addEventListener("click", selectAnswer);
+    answerButtonsElement.appendChild(button);
+  });
 }
 
 function resetState() {
-  clearStatusClass(document.body)
-  nextButton.classList.add('hide')
+  clearStatusClass(document.body);
+  nextButton.classList.add("hide");
   while (answerButtonsElement.firstChild) {
-    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild);
   }
 }
 
 function selectAnswer(e) {
-  const selectedButton = e.target
-  const correct = selectedButton.dataset.correct
-  setStatusClass(document.body, correct)
-  Array.from(answerButtonsElement.children).forEach(button => {
-    setStatusClass(button, button.dataset.correct)
-  })
+  const selectedButton = e.target;
+  const correct = selectedButton.dataset.correct;
+  setStatusClass(document.body, correct);
+  Array.from(answerButtonsElement.children).forEach((button) => {
+    setStatusClass(button, button.dataset.correct);
+  });
   if (shuffledQuestions.length > currentQuestionIndex + 1) {
-    nextButton.classList.remove('hide')
-    clicks++
+    nextButton.classList.remove("hide");
+    clicks++;
     queCounter.innerHTML = `
     Question ${clicks} / ${questions.length}
   `;
   } else {
-    startButton.innerText = 'Restart'
-    clicks = 1
+    startButton.innerText = "Restart";
+    clicks = 1;
     queCounter.innerHTML = `
     Question ${clicks} / ${questions.length}
   `;
-    startButton.classList.remove('hide')
+    startButton.classList.remove("hide");
   }
 }
 
 function setStatusClass(element, correct) {
-  clearStatusClass(element)
+  clearStatusClass(element);
   if (correct) {
-    element.classList.add('correct')
+    element.classList.add("correct");
   } else {
-    element.classList.add('wrong')
+    element.classList.add("wrong");
   }
 }
 
 function clearStatusClass(element) {
-  element.classList.remove('correct')
-  element.classList.remove('wrong')
+  element.classList.remove("correct");
+  element.classList.remove("wrong");
 }
-
