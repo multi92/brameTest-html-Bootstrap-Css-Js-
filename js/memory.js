7e107f47042cf3ee89a0836e056731975f3a08bb
Milos Mijajlovic <milos92mijajlovic@gmail.com>
2022-08-18 10:56:17 +0200

    memory.js logic updated

--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,62 @@
 const cards = document.querySelectorAll(".memory-card");
+//when player click card(if first card or second)
+
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
 
 function flipCard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
   //if clasesse here remowe it, if classes is not here added
-  this.classList.toggle("flip");
+  this.classList.add("flip");
+
+  //if player clicked first time card->
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+    return;
+  }
+  //second click
+  hasFlippedCard = false;
+  secondCard = this;
+  checkForMatch();
 }
 
-//lopp throw cards
+function checkForMatch() {
+  let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+  isMatch ? disableCards() : unfliCards();
+}
+
+function disableCards() {
+  //it's a match!!
+  firstCard.removeEventListener("click", flipCard);
+  secondCard.removeEventListener("click", flipCard);
+
+  resetBoard();
+}
+
+function unfliCards() {
+  lockBoard = true;
+  setTimeout(() => {
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
+    resetBoard();
+  }, 1500);
+}
+
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard.secondCard] = [null, null];
+}
+
+//immediatly inwoke function expresion
+(function shuffle() {
+  cards.forEach((card) => {
+    let randomPos = Math.floor(Math.random() * 12);
+    card.style.order = randomPos;
+  });
+})();
+
+//LOPP THROW THE CARDS
 cards.forEach((card) => card.addEventListener("click", flipCard));
