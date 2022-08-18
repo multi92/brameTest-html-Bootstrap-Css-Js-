993e317f8beab38300c43b4be7c146eb9a168bd3
Milos Mijajlovic <milos92mijajlovic@gmail.com>
2022-08-18 12:29:29 +0200

    memory.js countdown

--- js/memory.js
+++ js/memory.js
@@ -1,5 +1,26 @@
 const cards = document.querySelectorAll(".memory-card");
+const countdownEl = document.getElementById("countdown");
 //when player click card(if first card or second)
+
+///COUNTDOWN FUNCTION
+const startingMinutes = 0.3;
+let time = startingMinutes * 60;
+
+setInterval(updateCountdown, 300);
+
+function updateCountdown() {
+  const minutes = Math.floor(time / 60);
+  let seconds = time % 60;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+  countdownEl.innerHTML = `${minutes} : ${seconds}`;
+  time--;
+
+  if (time === 0) {
+    alert("time expected");
+    time++;
+  }
+}
+///COUNTDOWN FUNCTION END
 
 let hasFlippedCard = false;
 let lockBoard = false;
@@ -50,7 +71,7 @@ function resetBoard() {
   [firstCard.secondCard] = [null, null];
 }
 
-//immediatly inwoke function expresion
+//immediatly inwoke function expresion executed right after definition
 (function shuffle() {
   cards.forEach((card) => {
     let randomPos = Math.floor(Math.random() * 12);
